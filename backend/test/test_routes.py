c8834292bdc8f61e157171f2c27d9bc86c44ee77
yamazaki chihiro <ddi_00012@dk.daihatsu.co.jp>
2025-06-27 16:35:14 +0900

    ユーザー登録から メールアドレス関係をコメントアウト

--- backend/test/test_routes.py
+++ backend/test/test_routes.py
@@ -6,15 +6,17 @@ from database import get_db
 router = APIRouter()
 
 # ユーザー一覧取得（GET）ß
-@router.get("/users", response_model=list[test_schemas.UserRead])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return test_cruds.get_users(db, skip=skip, limit=limit)
+# @router.get("/users", response_model=list[test_schemas.UserRead])
+# def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+#     return test_cruds.get_users(db, skip=skip, limit=limit)
 
 # ユーザー登録（POST）
 @router.post("/users", response_model=test_schemas.UserRead)
 def create_user(user: test_schemas.UserCreate, db: Session = Depends(get_db)):
-    # email重複チェック（任意で）
-    existing = db.query(test_models.User).filter(test_models.User.email == user.email).first()
-    if existing:
-        raise HTTPException(status_code=400, detail="Email already registered")
     return test_cruds.create_user(db, user)
+
+    # email重複チェック（任意で）
+    # existing = db.query(test_models.User).filter(test_models.User.email == user.email).first()
+    # if existing:
+    #     raise HTTPException(status_code=400, detail="Email already registered")
+    # return test_cruds.create_user(db, user)
