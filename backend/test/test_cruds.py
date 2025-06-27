a2c58a6d81af047c54d3a231957dbfbeb53661a9
yamazaki chihiro <ddi_00012@dk.daihatsu.co.jp>
2025-06-27 16:43:36 +0900

    新規ユーザーに登録する内容の修正

--- backend/test/test_cruds.py
+++ backend/test/test_cruds.py
@@ -1,15 +1,27 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from test.test_models import User
 from test.test_schemas import UserCreate
 
 # ユーザー新規登録
 def create_user(db: Session, user: UserCreate):
-    db_user = User(name=user.name, email=user.email)
-    db.add(db_user)
+    # 社員番号の重複チェック
+    existing_user = db.query(User).filter(User.employee_code == user.employee_code).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="この社員コードはすでに登録されています")
+    
+    # ユーザーの新規登録
+    new_user = User(
+        nickname=user.nickname, 
+        employee_code=user.employee_code,
+        admin=user.admin
+        )
+
+    db.add(new_user)
     db.commit()
-    db.refresh(db_user)
-    return db_user
+    db.refresh(new_user)
+    return new_user
 
 # ユーザー一覧取得
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(User).offset(skip).limit(limit).all()
+# def get_users(db: Session, skip: int = 0, limit: int = 100):
+#     return db.query(User).offset(skip).limit(limit).all()
