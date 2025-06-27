6384ebd91494719e9d870a39bed4810ecc21c823
yamazaki chihiro <ddi_00012@dk.daihatsu.co.jp>
2025-06-27 19:18:16 +0900

    test用にcreate_app() を作成

--- backend/main.py
+++ backend/main.py
@@ -3,20 +3,28 @@ from database import engine, Base
 from routes import router
 from fastapi.middleware.cors import CORSMiddleware
 
+def create_app():
+    
+    #FastAPIアプリケーションのインスタンスを作成する関数
+   
+    # テーブル作成（初回のみ自動で作るパターン）
+    Base.metadata.create_all(bind=engine)
 
-# テーブル作成（初回のみ自動で作るパターン）
-Base.metadata.create_all(bind=engine)
+    app = FastAPI()
 
-app = FastAPI()
+    # CORS設定（開発時は全許可でもOK）
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-# CORS設定（開発時は全許可でもOK）
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    # ルーターを登録
+    app.include_router(router)
+    
+    
+    return app
 
-# ルーターを登録
-app.include_router(router)
+app = create_app()
